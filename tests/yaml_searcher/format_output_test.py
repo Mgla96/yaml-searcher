c1f9577eb6126d242964fc7a3b64a8f13ed6edc9
mgla96 <mattgottlieb96@gmail.com>
2022-05-29 13:12:51 -0700

    feat: unit tests checking format_output func with int and float

--- tests/yaml_searcher/format_output_test.py
+++ tests/yaml_searcher/format_output_test.py
@@ -34,3 +34,27 @@ def test_format_output_arr_success():
 
     # assert
     assert actual == expected
+
+
+def test_format_output_int_success():
+    # arrange
+    input = 5
+    expected = 5
+
+    # act
+    actual = format_output(input)
+
+    # assert
+    assert actual == expected
+
+
+def test_format_output_float_success():
+    # arrange
+    input = 5.55
+    expected = 5.55
+
+    # act
+    actual = format_output(input)
+
+    # assert
+    assert actual == expected
